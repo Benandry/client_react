a12f843cf393c8561c51dcd68465d051e4959aac
Eloi Charly <nandry556@gmail.com>
2023-09-20 11:19:11 +0200

    fix:Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { FC } from "react";
-import "./App.css";
 
 const App: FC = () => {
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut possimus,
+        hic libero iste ducimus nihil cumque. Quam sequi cum illum delectus qui
+        inventore quo dignissimos repudiandae incidunt? Ullam, magnam suscipit?
+      </p>
     </div>
   );
 };
